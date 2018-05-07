a6fca15454518da05abdedbfbadfb60b6df92bd4
Priswall <31701242+Priswall@users.noreply.github.com>
2018-05-07 11:28:49 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+var cc = document.createElement("canvas");
+cc.width = window.innerWidth;
+cc.height = window.innerHeight;
+cc.id = "canavs";
+document.body.appendChild(cc);
+
+var canavs = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+var keys = [];
+var s = "menu"
+
+function draw() {
+
+  if(s === "menu") {
+  
+    c.fillStyle = "black";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.drawImage(isaacSprite, 0, 0);
+  
+  }
+
+  requestAnimationFrame(draw);
+
+}
+
+requestAnimationFrame(draw);
+addEventListener("keydown", function(e) {
+
+  keys[e.keyCode] = true;
+
+})
+addEventListener("keyup", function(e) {
+
+  keys[e.keyCode] = false;
+
+})
