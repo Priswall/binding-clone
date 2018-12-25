82ba103c5cb89e4d4f1c75358067ef4d0aa62c00
Priswall <31701242+Priswall@users.noreply.github.com>
2018-12-25 18:21:33 -0500

    Update Sprite.js

--- Sprite.js
+++ Sprite.js
@@ -111,8 +111,9 @@ function initIsaac(img) {
 var isaacSprite = new Image(224, 124);
 isaacSprite.onload = function() {
   initIsaac(this);
-  var script = document.createElement(script);
-  script.src = "Player.js";
-  document.body.appendChild(script);
 };
+
 isaacSprite.src = "res/isaac.png";
+var script = document.createElement(script);
+script.src = "Player.js";
+document.body.appendChild(script);
