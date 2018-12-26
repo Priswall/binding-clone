f6f871d8a079c0289cffd196a251a1fe85fdb2e2
Priswall <31701242+Priswall@users.noreply.github.com>
2018-12-25 21:57:47 -0500

    Update parser.js

--- res/roomlang/parser.js
+++ res/roomlang/parser.js
@@ -10,17 +10,32 @@ function floorDoc() {
       if (rawFile.readyState === 4) {
         var allText = rawFile.responseText;
         this.commands = allText.split("\n");
-        var string = document.createElement("h1");
-        string.innerHTML = this.commands[0];
-        string.style.color = "white";
-        document.body.appendChild(string);
       }
     }
     rawFile.send();
   }
   
-  this.createRoom = function() {
-    
+  this.createRoom = function(room) {
+    var result = [];
+    for(var i = 0; i < this.commands.length; i++) {
+      if(this.commands[i][0] === "[") {
+        var temp = this.commands[i].replace(/[\[\]]+/g, "");
+        room.name = temp;
+        continue;
+      }
+      result = this.commands[i].split(" ");
+    }
+  } 
+  for(var i = 0; i < result.length; i++) {
+    switch(result[i]) {
+      case "tile":
+        switch(result[i + 1]) {
+          case 0:
+            tiles.push(new Rock(room.pos.x + (74 + (result[i + 2] * 23.2)), room.pos.y + (46 + (result[i + 3] * 23.2))));
+            break;
+        }
+        break;
+    }
   }
 }
 
