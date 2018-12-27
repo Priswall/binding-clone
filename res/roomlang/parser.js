9fb9beadfb0e943d6e5cded4cd1eeed51268c7f3
Priswall <31701242+Priswall@users.noreply.github.com>
2018-12-26 23:51:01 -0500

    Update parser.js

--- res/roomlang/parser.js
+++ res/roomlang/parser.js
@@ -1,55 +1,41 @@
 function floorDoc() {
   this.fileName = "";
-  this.commands = ["g"];
+  this.commands = [];
   
   this.load = function(fileName) {
     this.fileName = fileName;
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "res/rooms/basement.floor", true);
-    rawFile.onreadystatechange = function() {
-      if (rawFile.readyState === 4) {
-        var allText = String(rawFile.responseText).replace(/[\[\]]+/g, "");
-        this.commands = allText.split("\n");
-
-        var script = document.createElement("h1");
-        script.innerHTML = this.commands;
-        script.style.color = "white";
-        document.body.appendChild(script);
-
-        var script = document.createElement("script");
-        script.src = "Sprite.js";
-        document.body.appendChild(script);
-      }
-    }
-    rawFile.send();
   }
   
   this.createRoom = function(room) {
     var result = [];
-
-    var script = document.createElement("h1");
-    script.innerHTML = this.commands;
-    script.style.color = "white";
-    document.body.appendChild(script);
     
-    for(var i = 0; i < this.commands.length; i++) {
-      var temp = this.commands[0].replace(/\"/, "");
-      room.name = String(temp);
-      if(this.commands[i] !== "\n")
-        result.push(this.commands[i].split(" "));
-    }
-    room.name = String(this.commands) + String(Math.random());
-    for(var i = 0; i < result.length; i++) {
-      switch(String(result[i])) {
-        case "tile":
-          switch(Number(result[i + 1])) {
-            case 0:
-              room.tiles.push(new Rock(room.pos.x + (74 + (Number(result[i + 2]) * 23.2)), room.pos.y + (46 + (Number(result[i + 3]) * 23.2))));
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", this.fileName, true);
+    rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4) {
+        var allText = String(rawFile.responseText).replace(/[\[\]]+/g, "");
+        this.commands = allText.split("\n");
+        for(var i = 0; i < this.commands.length; i++) {
+          var temp = this.commands[0].replace(/\"/, "");
+          room.name = String(temp);
+          if(this.commands[i] !== "\n")
+            result.push(this.commands[i].split(" "));
+        }
+        room.name = String(this.commands) + String(Math.random());
+        for(var i = 0; i < result.length; i++) {
+          switch(String(result[i])) {
+            case "tile":
+              switch(Number(result[i + 1])) {
+                case 0:
+                  room.tiles.push(new Rock(room.pos.x + (74 + (Number(result[i + 2]) * 23.2)), room.pos.y + (46 + (Number(result[i + 3]) * 23.2))));
+                  break;
+              }
               break;
           }
-          break;
+        }
       }
     }
+    rawFile.send();
   } 
 }
 
