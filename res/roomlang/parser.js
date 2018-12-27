4332393c92654bdfa6ec0a880a9973dd82af68de
Priswall <31701242+Priswall@users.noreply.github.com>
2018-12-26 23:04:05 -0500

    Update parser.js

--- res/roomlang/parser.js
+++ res/roomlang/parser.js
@@ -21,7 +21,7 @@ function floorDoc() {
   
   this.createRoom = function(room) {
     var result = [];
-    room.name = String(this.commands[0]);
+    room.name = this.commands[0];
     for(var i = 0; i < this.commands.length; i++) {
       if(this.commands[i][0] === "[") {
         var temp = this.commands[i].replace(/[\[\]]+/g, "");
