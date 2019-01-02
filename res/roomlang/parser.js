dfb9e061e093e990dab545a6568950642b9bff21
Priswall <31701242+Priswall@users.noreply.github.com>
2019-01-02 14:57:18 -0500

    Update parser.js

--- res/roomlang/parser.js
+++ res/roomlang/parser.js
@@ -13,21 +13,21 @@ function floorDoc() {
     rawFile.open("GET", this.fileName, true);
     rawFile.onreadystatechange = function() {
       if (rawFile.readyState === 4) {
-        var allText = String(rawFile.responseText).replace(/[\[\]]+/g, "");
+        var allText = String(rawFile.responseText);
         this.commands = allText.split("\n");
         for(var i = 0; i < this.commands.length; i++) {
-          var temp = this.commands[0].replace(/\"/, "");
+          var temp = this.commands[0].replace(/[\[\]]+/g, "");
           room.name = String(temp);
           if(this.commands[i] !== "\n")
             result.push(this.commands[i].split(" "));
         }
-        room.name = String(this.commands) + String(Math.random());
         for(var i = 0; i < result.length; i++) {
           switch(String(result[i])) {
             case "tile":
               switch(Number(result[i + 1])) {
                 case 0:
                   room.tiles.push(new Rock(room.pos.x + (74 + (Number(result[i + 2]) * 23.2)), room.pos.y + (46 + (Number(result[i + 3]) * 23.2))));
+                  console.log("TESTING!!!");
                   break;
               }
               break;
